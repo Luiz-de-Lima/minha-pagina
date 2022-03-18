70718a9e4441b60df660ebfce4b35c015d0d8ed7
Luiz de Lima <luiz12359@gmail.com>
2022-03-18 02:52:33 -0300

    adição do js para futura validação

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const labels = document.querySelectorAll('.form-control label')
+console.log(labels)
+// labels.forEach((label) => {
+//     label.addEventListener('click', () => {
+//         label.classList.add('top')
+//         console.log(label)
+//     })
+//     label.classList.remove('top')
+// })
+
+// labels.forEach((label) => {
+//     label.innerHTML = label.innerText.split('').map((letter) =>
+//         console.log(letter),
+//         `<span style=" transition-delay:${index * 50}ms">${letter}
+//     </span>`).join('')
+// })
